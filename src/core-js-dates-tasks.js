e24df25de75ec52a5cb367c3d25400dab6c570bc
Nekopunch <Nekopunch2482@yandex.ru>
2025-02-25 18:41:07 +0300

    feat: implement getNextFriday

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -67,8 +67,14 @@ function getDayName(date) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const d = new Date(date);
+  const day = d.getDay();
+
+  const diff = day < 5 ? 5 - day : 12 - day;
+  d.setDate(d.getDate() + diff);
+
+  return d;
 }
 
 /**
